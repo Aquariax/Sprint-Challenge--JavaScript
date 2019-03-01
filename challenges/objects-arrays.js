c9173da0d2612cadd5ded051ec1b1593e70d4844
Aquariax <roberthrrs8@gmail.com>
2019-03-01 14:07:49 -0500

    mostly complete sprint challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,30 +5,64 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+function Dinosaur(dinoAttr){
+  this.name = dinoAttr.name;
+  this.diet = dinoAttr.diet;
+  this.weight = dinoAttr.weight;
+  this.length = dinoAttr.length;
+  this.period = dinoAttr.period;
+}
+
+const trex = new Dinosaur({
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceious"
+})
+
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+const stegosaurus = new Dinosaur({
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+})
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+
+const velociraptor = new Dinosaur({
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious"
+})
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
-
+Dinosaur.prototype.roar= function(){
+  console.log(`RAWERSRARARWERSARARARRRR!`)
+}
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
@@ -89,6 +123,10 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(function(zooAnimals){
+  const dell = (`Animal: ${zooAnimals.animal_name}, Scientific: ${zooAnimals.scientific_name}`);
+  animalNames.push(dell) ;
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -97,7 +135,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(function(zooAnimals){
+  return zooAnimals.animal_name.toLowerCase();
+});
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -105,7 +145,9 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(function(zooAnimals){
+  return zooAnimals.population < 5
+});;
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -113,7 +155,10 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal =  zooAnimals.reduce(function(amount, zooAnimals){
+  return amount + zooAnimals.population;
+}, 0);
+
 console.log(populationTotal);
 
 
