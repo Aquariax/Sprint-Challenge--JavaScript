b22f0f1592b907e8a7f527b31b51359783b4ae0c
Aquariax <roberthrrs8@gmail.com>
2019-03-01 14:30:22 -0500

    finished mvp by adding three for loops for missing functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -84,20 +84,34 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i =0; i < graduates.length; i++){
+  const tee = (graduates[i].university)
+  universities.push(tee) 
+}
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
 The resulting contact information should have a space between the first name and the email information like this: 
 Name email@example.com
 
+
 Log the result of your new array. */
 const contactInfo = [];
+for (let i =0; i < graduates.length; i++){
+  const dom = (`${graduates[i].first_name} ${graduates[i].email}`)
+  contactInfo.push(dom) 
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i =0; i < graduates.length; i++){
+  if(graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university)
+  }
+}
 console.log(uni);
 
 
